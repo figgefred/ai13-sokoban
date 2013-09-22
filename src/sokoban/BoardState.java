b25f38e0417f7c4b368baecb91c6e44851b5e3b0
Joakim Uddholm <joakim@uddholm.com>
2013-09-22 22:11:38 +0200

    Lite funktioner för att flytta spelaren (tills vidare, måste kunna ändra boardstate?)

--- src/sokoban/BoardState.java
+++ src/sokoban/BoardState.java
@@ -12,6 +12,9 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
+
+import com.sun.corba.se.impl.orbutil.graph.Node;
+
 import sokoban.Algorithms.ISearchAlgorithmPath;
 
 /**
@@ -141,6 +144,92 @@ public class BoardState
     {
         return Goals;
     }
+    
+    
+    /***
+     * Moves the player to target position. Resetting the old position to its normal value.
+     * Throws exception if the move is invalid.
+     * 
+     * Todo: Maybe add function to move player in a direction?
+     * @param x
+     * @param y
+     */
+    public void movePlayerTo(int x, int y)
+    {
+    	if(x < 0 || x >= Map.size())
+    		throw new IllegalArgumentException("Position is out of bounds (x = " + x + ")");
+    
+    	int diff = x - CurrentNode.Column + y - CurrentNode.Row;
+    	if(Math.abs(diff) > 1)
+    		throw new IllegalArgumentException("Can't move player more than one coordinate");
+    	
+    	NodeType type = Map.get(y).get(x);
+		if(type == NodeType.BLOCK || type == NodeType.BLOCK_ON_GOAL)
+		{
+			// Check if block can be pushed	(and if so do so)	
+			if(diff > 0) // North, west
+			{
+				if(y > CurrentNode.Row) // North
+					pushBlock(x, y, x, y+1);
+				else
+					pushBlock(x, y, x+1, y);
+			}
+			else { // south, east
+				if(y < CurrentNode.Row) // South
+					pushBlock(x, y, x, y-1);
+				else
+					pushBlock(x, y, x-1, y);
+			}
+		
+		} else if(type != NodeType.SPACE) {
+			// Wall or other invalid move
+			throw new IllegalArgumentException("Invalid move");			
+		}
+		
+		// Set new position
+		Map.get(y).set(x, type == NodeType.GOAL ? NodeType.PLAYER_ON_GOAL : NodeType.PLAYER);    	
+    	
+		// Reset old position
+    	NodeType playerType = Map.get(CurrentNode.Row).get(CurrentNode.Column);
+		Map.get(CurrentNode.Row).set(CurrentNode.Column, (playerType == NodeType.PLAYER) ? NodeType.SPACE : NodeType.GOAL);
+	
+    }
+    
+    private void pushBlock(int x, int y, int newx, int newy) {
+    	NodeType orig = Map.get(y).get(x);
+    	NodeType dest = Map.get(newy).get(newx);
+    	
+    	if(orig != NodeType.BLOCK && orig != NodeType.BLOCK_ON_GOAL)
+    		throw new IllegalArgumentException("Push was called to push non-block: " + orig.toString());
+    	if(dest != NodeType.GOAL && dest != NodeType.SPACE)
+    		throw new IllegalArgumentException("Can't push block, something is in the way: " + dest.toString());    	
+    	
+    	Map.get(y).set(x, (orig == NodeType.BLOCK_ON_GOAL) ? NodeType.GOAL : NodeType.SPACE);
+    	
+    	Map.get(newy).set(newx, (dest == NodeType.GOAL) ? NodeType.BLOCK_ON_GOAL : NodeType.BLOCK);
+    }
+    
+    /***
+     * Moves the player to target position. Resetting the old position to its normal value
+     * @param p
+     */
+    public void movePlayerTo(BoardPosition p)
+    {
+    	movePlayerTo(p.Row, p.Column);
+    }
+    
+    /***
+     * Basic check if the game is won. Checks if all goals are occupied by a block.
+     * @return
+     */
+    public boolean isWin() {
+    	for(BoardPosition goal : Goals)
+    	{
+    		if(getNode(goal) != NodeType.BLOCK_ON_GOAL)
+    			return false;
+    	}
+    	return true;
+	}
     
 
 }
