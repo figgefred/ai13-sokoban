acdcfb64377826a088e9065111ca6fd45733db2b
Frederick Ceder <frederick@ceders.net>
2013-10-16 01:41:18 +0200

    fredmaster2: Lagt till funktion i LiveAnalyser som hittar Corralytor. Är dock låååångsam. Finns utrymme för optimering. Får ser vad det bli imorrn

---
+++ src/sokoban/fredmaster2/CorralFenceCandidate.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sokoban.fredmaster2;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import sokoban.BoardPosition;
+
+/**
+ * A class to temporarily buffer a single Block-type BoardPosition before potentially entering it
+ * as a FenceNode into areas. 
+ * 
+ * The simple idea is:
+ *  - Several areas can be related to one position
+ *  - If more then one area 'touches' a single block node, then this is with great
+ *    probability a fence node.
+ * 
+ * See what isPartOfCorralAreaFence() returns
+ * 
+ * @author figgefred
+ */
+public class CorralFenceCandidate {
+    
+    private Set<Area> AreasCovered;
+    private BoardPosition Position;
+    
+    /**
+     * It is assumed that this BoardPosition points to a BlockNode type BoardPosition
+     * @param pos 
+     */
+    public CorralFenceCandidate(BoardPosition pos)
+    {
+        Position = pos;
+        AreasCovered = new HashSet<>();
+    }
+    
+    /**
+     * Add an area that 'touches' this block node BoardPosition
+     * @param area 
+     */
+    public void addCorralArea(Area area)
+    {
+        AreasCovered.add(area);
+    }
+    
+    public BoardPosition getBoardPosition()
+    {
+        return Position;
+    }
+    
+    public Set<Area> getAreasCovered()
+    {
+        return AreasCovered;
+    }
+    
+    public boolean isNodeOf(Area area)
+    {
+        return AreasCovered.contains(area);
+    }
+    
+    /**
+     * If more then one area 'touch' this Candidate, then this is a fence node
+     * @return 
+     */
+    public boolean isPartOfCorralAreaFence()
+    {
+        return (AreasCovered.size() > 1);
+    }
+    
+}
